f2a0a6d09d8b95416096eb7be42c2e75e0f0860d
rajukodoori0405-lab <rajukodoori0405@gmail.com>
2026-03-01 14:37:28 +0530

    commit commit

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -3,53 +3,8 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-  # self is the special variable
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > inventory.ini"
-  }
-
-  provisioner "local-exec" {
-    command = "exit 1"
-    on_failure = continue
-  }
-
-  provisioner "local-exec" {
-    command = "echo script-2"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "echo 'Deleting the instance'"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "echo > inventory.ini"
-  }
-
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo dnf install nginx -y",
-      "sudo systemctl start nginx"
-    ]
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo systemctl stop nginx"
-    ]
-    when = destroy
-  }
-
   tags = {
-    Name = "provisioners-demo"
+    Name = "terraform-state"
     Project = "roboshop"
   }
 }
